15831e621530edb6b1df35322297fb917a4344a8
sergiosaruijr <sergiosaruijr@gmail.com>
2025-05-30 14:58:35 -0300

    feat: create page container components

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -11,10 +11,9 @@ import {
 import { Nunito } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 import { Avatar } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -67,6 +66,7 @@ const items = [
 export function AppSidebar() {
   const router = useRouter();
   const session = authClient.useSession();
+  const pathname = usePathname();
 
   const handleSignOut = async () => {
     await authClient.signOut({
@@ -93,7 +93,7 @@ export function AppSidebar() {
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton asChild isActive={pathname === item.url}>
                     <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
