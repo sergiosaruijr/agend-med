5f708c1827a2bd54a099f4f44c4b269ed5aec7de
sergiosaruijr <sergiosaruijr@gmail.com>
2025-05-29 23:20:10 -0300

    feat: add clinic form page

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -12,8 +12,8 @@ const AuthenticationPage = async () => {
     headers: await headers(),
   });
 
-  if (!session?.user) {
-    redirect("/authentication/");
+  if (session?.user) {
+    redirect("/dashboard");
   }
 
   return (
---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,29 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import ClinicForm from "./components/form";
+
+const ClinicFormPage = () => {
+  return (
+    <div>
+      <Dialog open={true}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Adicionar Clínica</DialogTitle>
+            <DialogDescription>
+              Adicione uma clínica para que você possa gerenciar seus pacientes
+            </DialogDescription>
+          </DialogHeader>
+          <ClinicForm />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default ClinicFormPage;
