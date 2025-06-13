f9b6dcbfd46f145adadc6e6b7a0b0deddb1e4ddb
sergiosaruijr <sergiosaruijr@gmail.com>
2025-06-13 12:20:52 -0300

    feat: add patients table with edit actions

---
+++ src/app/(protected)/patients/_components/table-actions.tsx
@@ -0,0 +1,61 @@
+import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Dialog } from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { patientsTable } from "@/db/schema";
+
+import UpsertPatientForm from "./upsert-patient-form";
+
+interface PatientsTableActionsProps {
+  patient: typeof patientsTable.$inferSelect;
+}
+
+const PatientsTableActions = ({ patient }: PatientsTableActionsProps) => {
+  const [upsertDialogIsOpen, setUpsertDialogIsOpen] = useState(false);
+  const router = useRouter();
+
+  return (
+    <Dialog open={upsertDialogIsOpen} onOpenChange={setUpsertDialogIsOpen}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon">
+            <MoreVerticalIcon className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => setUpsertDialogIsOpen(true)}>
+            <EditIcon />
+            Editar
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <TrashIcon />
+            Excluir
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <UpsertPatientForm
+        isOpen={upsertDialogIsOpen}
+        patient={patient}
+        onSuccess={() => {
+          setUpsertDialogIsOpen(false);
+          router.refresh();
+        }}
+      />
+    </Dialog>
+  );
+};
+
+export default PatientsTableActions;
